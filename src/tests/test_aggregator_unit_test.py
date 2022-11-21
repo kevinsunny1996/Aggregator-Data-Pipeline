a3960dd0f1e8f2a4844ba669f1f46fd3fbeae290
Kevin Parasseril <kevinsunny1996@gmail.com>
2022-11-21 17:19:45 +0530

    added self

--- src/tests/test_aggregator_unit_test.py
+++ src/tests/test_aggregator_unit_test.py
@@ -9,11 +9,11 @@ from src.location_fetcher import get_locations
 
 class TestResultLength:
     # Check to confirm that the list size of the metadata method is same and not less than 46 to make sure zip function works in locations module
-    def test_metadata_count() -> None:
+    def test_metadata_count(self) -> None:
         site_ids, language_codes, locales = get_metadata()
         assert len(site_ids) == len(language_codes) == len(locales) == 46
 
     # Check to make sure that the hotel listings are not empty and that they are of equal length
-    def test_hotel_listings_count() -> None:
+    def test_hotel_listings_count(self) -> None:
         city_ids, property_ids = get_locations()
         assert len(city_ids) == len(property_ids) != 0
