a50d1001deba40d9f3104f7d6d8e0d3d1b5019e7
Kevin Parasseril <kevinsunny1996@gmail.com>
2022-12-11 15:59:45 +0530

    Added config for scopes and created test case for scope check

---
+++ src/scope_fetcher.py
@@ -0,0 +1,59 @@
+import json
+
+# Logger factory class import
+from logger.logger import LoggerFactory
+
+
+class ScopeGenerator():
+    def __init__(self):
+        super()
+
+    @staticmethod()
+    def get_scopes_per_request(self, scope_id: str) -> str:
+        api_scopes = open('configs/web_api_scopes.json', 'r')
+        scopes     = json.load(api_scopes)
+        LoggerFactory.get_logger('./logs/info.log', INFO).info(f'Fetching scope : {scopes[self.scope_id]} for scope_id: {self.scope_id}')
+        return scopes[self.scope_id]
+
+
+# def get_locations() -> []:
+
+#     site_ids, language_codes, locales = get_metadata() 
+
+#     location_dict = open('configs/location_query.json', 'r')
+#     location = json.load(location_dict)
+
+#     location_url = f"https://{location['headers']['x_rapidapi_host']}/{location['endpoints']['location_url']}"
+
+#     location_headers = {
+#         "X-RapidAPI-Key": location['headers']['x_rapidapi_key'],
+#         "X-RapidAPI-Host": location['headers']['x_rapidapi_host']
+#     }
+
+#     for site_id, lang_id, locale in zip(site_ids, language_codes, locales):
+#         try:
+#             locations_querystring = {"q":"New York","locale":locale,"langid":lang_id,"siteid":site_id}
+#             LoggerFactory.get_logger('./logs/info.log', 'INFO').info(f'Running GET request for locale: {locale}, language ID: {lang_id}, site ID: {site_id}')
+#             locations_res = requests.request("GET", location_url, headers=location_headers, params=locations_querystring).json()
+#         except Exception as e:
+#             LoggerFactory.get_logger('./logs/error.log', 'ERROR').error(f'Error in invoking query : {location_url} , recieved the following error : {e}')    
+
+#     city_ids = []
+#     property_ids = []
+
+#     try:
+#         for location in range(len(locations_res['sr'])):
+#             if locations_res['sr'][location]['type'] == 'HOTEL':
+#                 city_ids.append(locations_res['sr'][location]['cityId'])
+#                 property_ids.append(locations_res['sr'][location]['hotelId'])
+#     except Exception as e:
+#         LoggerFactory.get_logger('./logs/error.log', 'ERROR').error(f'Error in iterating json response recieved the following error : {e}')
+
+
+
+#     try:
+#         LoggerFactory.get_logger('./logs/info.log', 'INFO').info(f'Retrieved {len(city_ids)} city IDs , {len(property_ids)} property IDs from given locations url {location_url}')
+#     except Exception as e:
+#         LoggerFactory.get_logger('./logs/error.log', 'ERROR').error(f'Error in invoking query : {location_url} , recieved the following error : {e}')
+
+#     return city_ids, property_ids
--- src/tests/test_aggregator_unit_test.py
+++ src/tests/test_aggregator_unit_test.py
@@ -1,19 +1,13 @@
 import pytest
 import sys
+import json
 
 sys.path.append('..')
 
-from src.metadata_fetcher import get_metadata
-from src.location_fetcher import get_locations
+from src.scope_fetcher import ScopeGenerator
 
 
-class TestResultLength:
-    # Check to confirm that the list size of the metadata method is same and not less than 46 to make sure zip function works in locations module
-    def test_metadata_count(self) -> None:
-        site_ids, language_codes, locales = get_metadata()
-        assert len(site_ids) == len(language_codes) == len(locales) == 46
-
-    # Check to make sure that the hotel listings are not empty and that they are of equal length
-    def test_hotel_listings_count(self) -> None:
-        city_ids, property_ids = get_locations()
-        assert len(city_ids) == len(property_ids) != 0
+# Check to confirm that the list size of the metadata method is same and not less than 46 to make sure zip function works in locations module
+def test_scope_fetching(self) -> None:
+    test_scope = ScopeGenerator.get_scopes_per_request(urpp)        
+    assert test_scope == "user-read-playback-position"
