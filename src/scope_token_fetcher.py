1adbb094889f30362a4b915964cf79eebc3618a4
Kevin Parasseril <kevinsunny1996@gmail.com>
2022-12-15 22:41:35 +0530

    Added bearer token generator code and comments

--- src/scope_token_fetcher.py
+++ src/scope_token_fetcher.py
@@ -1,17 +1,70 @@
 import json
+import awswrangler.secretsmanager as sm
+import requests
+import base64
 
 # Logger factory class import
 from logger.logger import LoggerFactory
 
+# URL Encoded Redirect URL needed for getting code to be passed in get_bearer_token
+url_encoded_redirect = 'https%3A%2F%2Fjsonurl.org%2F'
 
-class ScopeGenerator():
+# Client ID, Client Secret and Code fetched from Secrets Manager 
+client_id = sm.get_secret_json("spotifyClientID").get('spotify_client_id')
+client_secret = sm.get_secret_json("spotifyClientSecret").get('spotify_client_secret')
+code = sm.get_secret_json("spotifyAuthCode").get('spotify_auth_code')
+
+class ScopeAndTokenGenerator():
+    """
+    Contains methods to fetch scopes and bearer tokens.
+    """
+    
     def __init__(self, scope_id):
         self.scope_id = scope_id
+    
+    """
+    Title - Gets scopes based on key passed 
+    Description - The method takes in json doc and based on the key passed for each scoped mapped in json file , returns the value for it.
+
+    Returns
+    ------------------------------------------
+    Bearer Token : str 
+
+    Description - The extracted scopes can then be used to call various Spotify endpoints programatically
+    """
     def get_scopes_per_request(self) -> str:
         api_scopes = open('configs/web_api_scopes.json', 'r')
-        scopes     = json.load(api_scopes)
+        scopes = json.load(api_scopes)
         LoggerFactory.get_logger('./logs/info.log', 'INFO').info(f'Fetching scope : {scopes[self.scope_id]} for scope_id: {self.scope_id}')
         return scopes[self.scope_id]
+
+    
+    """
+    Title - Gets Bearer Token 
+    Description - The following method takes in encoded client_id and client_secret (Base-64) and fetches bearer and refresh token.
+
+    Returns
+    ------------------------------------------
+    Bearer Token : str 
+
+    Description - The token can then be used to call Spotify API Programatically
+    """
+    def get_bearer_token(self) -> str:
+        token_url = "https://accounts.spotify.com/api/token"
+        encoded_creds = base64.b64encode(client_id.encode() + b':' + client_secret.encode()).decode("utf-8")
+        token_headers = {"Authorization": f"Basic {encoded_creds}", "Content-Type": "application/x-www-form-urlencoded"}
+
+        token_data = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_url": "https://jsonurl.org/"
+        }
+
+        token_response = requests.post(token_url, data=token_data, headers=token_headers)
+
+        return token_response.json()["access_token"]
+
+
 
 
 # def get_locations() -> []:
