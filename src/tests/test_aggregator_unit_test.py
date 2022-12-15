a7864b4b18ba5e3272f34d0a88a0cfabce42c354
Kevin Parasseril <kevinsunny1996@gmail.com>
2022-12-15 22:43:21 +0530

    changed import name

--- src/tests/test_aggregator_unit_test.py
+++ src/tests/test_aggregator_unit_test.py
@@ -4,11 +4,11 @@ import json
 
 sys.path.append('..')
 
-from src.scope_fetcher import ScopeGenerator
+from src.scope_token_fetcher import ScopeGenerator
 
 
 # Check to confirm that the list size of the metadata method is same and not less than 46 to make sure zip function works in locations module
 def test_scope_fetching() -> None:
-    scope_gen_test = ScopeGenerator("urpp")
+    scope_gen_test = ScopeAndTokenGenerator("urpp")
     test_scope = scope_gen_test.get_scopes_per_request()        
     assert test_scope == "user-read-playback-position"
